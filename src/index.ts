351633e0fb13ad01aba2d303abb5e2b10aa773b2
Gerald <harvey.randall@aol.com>
2022-03-07 22:04:31 +0000

    Removed some whitespaces

--- src/index.ts
+++ src/index.ts
@@ -46,15 +46,16 @@
                                                                             
 `;
 
-import { PlayerJoinHook } from 'events/PlayerJoins';
-import { SalesHook } from 'events/Robux';
-import { serverVitalsHook } from 'events/ServerVitals';
 // RovolutionAnalytica is a library for reporting data to the RovolutionLogistcs API, for everything from visits to robux generated and much more. --
 
 import { setupGlobals } from 'globals';
+import { RL_LOG } from 'utils/consoleLogging';
+import { PlayerJoinHook } from 'events/PlayerJoins';
+import { SalesHook } from 'events/Robux';
+import { serverVitalsHook } from 'events/ServerVitals';
 
 export default function RovolutionAnalytica(projectID: string, apiKey: string) {
-    print('RovolutionAnalytica is starting up.');
+    RL_LOG('RovolutionAnalytica is starting up.');
 
     // First setup globals
     setupGlobals(projectID, apiKey);
